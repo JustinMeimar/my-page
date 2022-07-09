1b3ddde91c355cbd0880f950c88c553b86c2877d
Justin Meimar <j.meimar111@gmail.com>
2022-07-09 15:49:51 -0600

    upadte

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
     return(
         <div className="home_page"> 
             <div className="home_page_info">
-                Welcome to my page. <br/> <br/>This page is a work in progress. Im working on other projects,  to show my progress.
+                Welcome to my page. <br/> <br/>
             </div>   
            
         </div>     
