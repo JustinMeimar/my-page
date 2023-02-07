43ebf98ca6dbd007ed9564a6bc2e769117d601a2
Justin Meimar <j.meimar111@gmail.com>
2023-02-06 18:48:29 -0700

    changes to homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -6,32 +6,41 @@ function HomePage() {
         <div className="home_page"> 
             <div className="home_page_info">
             Hello, I’m Justin, a third year computer science student at 
-            <a href="https://www.ualberta.ca/computing-science/index.html"> the University of Alberta </a> 
-            who has a passion for programming and building projects from scratch. 
+            <a href="https://www.ualberta.ca/computing-science/index.html"> the University of Alberta </a>  
            
             <br></br> <br></br> 
-            I enjoy implementing low level programs and concepts. Some of my projects include 
-            a machine agnostic compiler, a virtual CPU, and an assembler for a custom ISA. I'm 
-            currently working on completing the projects described in 
-            <a href='https://github.com/geohot/fromthetransistor' target='_blank'> 
-             this Github blog</a>, 
-            which aims to teach the software stack from the ground up.
+            I enjoy implementing low level programs and concepts. A couple of my projects include 
+            a compiler for a vector languge which targets LLVM and a virtual CPU + assembler for 
+            a small toy ISA. I have some other stuff pinned on my GitHub. My primary love is for C++ 
+            at the moment.   
+
+            <br></br> <br></br>
+            I learned alot about the fundamentals of computers by implementing some of the suggested <a href='https://github.com/geohot/fromthetransistor' target='_blank'> 
+            this Github blog</a>, which I'm continuting to let guide my exploration.   
+           
+           
+            <br></br> <br></br>
+            Besides an interest in low-level, I have experience with modern 
+            tools in software development. I became close with Git 
+            working in multiple group projects ranging from 4 - 6 people. 
+            I'm familiar wth React after using it in a frontend for some 
+            simple CRUD apps (as well as this site) And proper database 
+            management theory from some CS classes, as well as skills from
+            my last internship. 
             
             <br></br> <br></br>
-            I believe that modern development relies on so many layers of abstraction that it's easy 
-            to lose sight of the fundamentals, so I enjoy demystifying the black boxes of computing. 
+            Last summer, I gained some real world experience working as a Full Stack 
+            at a <a href="https://bitsinglass.com/" target='_blank'>Bits In Glass</a>
+            , a local Edmonton company. There I learned the ropes of AGILE devleopment,
+            where we operated in two-week sprints. I helped my team in producing multiple iterations over the
+            application while during my time. 
             
             <br></br> <br></br>
-            In addition to an interest in low-level programming, I have experience with popular 
-            tools in modern software development, such as Git, React JS, and databases like 
-            MongoDB and MariaDB. Last summer, I gained some real world experience working as a Full Stack 
-            intern at a <a href="https://bitsinglass.com/" target='_blank'>Bits In Glass</a>
-            , a local Edmonton company.
-            
-            <br></br> <br></br>
-            Outside of computers, I enjoy reading and running. My <a href="https://www.goodreads.com/user/show/137002609-justin-meimar" 
-                target='_blank' >
-                 GoodReads</a> profile. 
+            Outside of computers, I enjoy reading, running and playing sports like hockey
+            and soccer. This is my   
+            <a href="https://www.goodreads.com/user/show/137002609-justin-meimar" 
+                target='_blank' > GoodReads</a> profile, though if im in school my 
+                book completion rate falls enormously.
             </div>    
         </div>     
     );
